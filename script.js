ce1f3f5fe9264c9cda41695943525920315c1eea
ctanamas <crystal.tanamas@uptake.com>
2022-09-17 12:14:37 -0400

    script

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Transciption
+//import 'dotenv/config';
+//import fetch from 'node-fetch';
+
+let id = 'o91jt8pmpb-b317-4568-928f-cb478c7a9bac';
+const url = `https://api.assemblyai.com/v2/transcript/${id}`;
+
+const params = {
+    headers: {
+      "authorization": "", //process.env.ASSEMBLYAI_API_KEY,
+      "content-type": "application/json",
+    },
+    method: 'GET'
+  };
+
 function getTranscript(id) {
-    console.log('test');
+
+
+    fetch(url, params)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data.text);
+   // print(data);
+    })
+    .catch((error) => {
+    console.error(`Error: ${error}`);
+    });
     // return 
 }
