0f59de11196156b072ae377ce22d4f136858d6cd
ctanamas <crystal.tanamas@uptake.com>
2022-09-17 10:59:23 -0400

    packages node-fetch and dotenv

---
+++ download.js
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import fetch from 'node-fetch';
+
+let args = process.argv.slice(2);
+let id = args[0];
+const url = `https://api.assemblyai.com/v2/transcript/${id}`;
+
+const params = {
+  headers: {
+    "authorization": process.env.ASSEMBLYAI_API_KEY,
+    "content-type": "application/json",
+  },
+  method: 'GET'
+};
+
+
+function print(data) {
+  switch (data.status) {
+    case 'queued':
+    case 'processing':
+      console.log('AssemblyAI is still transcribing your audio, please try again in a few minutes!');
+      break;
+    case 'completed':
+      console.log(`Success: ${data}`);
+      console.log(`Text: ${data.text}`);
+      break;
+    default:
+      console.log(`Something went wrong :-( : ${data.status}`);
+      break;
+  }
+}
+
+fetch(url, params)
+  .then(response => response.json())
+  .then(data => {
+    print(data);
+  })
+  .catch((error) => {
+    console.error(`Error: ${error}`);
+  });
